efef8f4bd61f20b9a19725ad4bda5be4bc9d64ec
Yichen112 <100495438+Yichen112@users.noreply.github.com>
2023-03-19 19:26:07 +0100

    Update hw_2_Challenge_Engine.sol

--- hw_2_Challenge_Engine.sol
+++ hw_2_Challenge_Engine.sol
@@ -1,9 +1,8 @@
-// SPDX-License-Identifier:MIT
-
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract BitwiseOperation {
-    function performBitwiseAND(uint a, uint b) public pure returns (uint) {
-        return a & b;
+contract Engine{
+    function bitwiseAnd(uint number_1, uint number_2) public pure returns (uint) {
+        return number_1 & number_2;
     }
 }
